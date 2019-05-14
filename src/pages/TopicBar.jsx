ab39543556e3d7fecf090ec419fce9d2266402a5
9080tom <t.thickett@hotmail.co.uk>
2019-05-14 10:00:18 +0100

    topic bar links

--- src/pages/TopicBar.jsx
+++ src/pages/TopicBar.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { getTopics } from "../components/api";
+import { Link } from "@reach/router";
 
 class TopicBar extends Component {
   state = {
@@ -9,15 +10,19 @@ class TopicBar extends Component {
     return (
       <div className="filter">
         <span>Topics</span>
-        <div>All</div>
+        <Link to="/">
+          <div>All</div>
+        </Link>
         {this.state.topics.map(topic => {
           return (
-            <div
-              key={this.ucfirst(topic.slug)}
-              value={this.ucfirst(topic.slug)}
-            >
-              {this.ucfirst(topic.slug)}
-            </div>
+            <Link to={`/home/${this.ucfirst(topic.slug)}`}>
+              <div
+                key={this.ucfirst(topic.slug)}
+                value={this.ucfirst(topic.slug)}
+              >
+                {this.ucfirst(topic.slug)}
+              </div>
+            </Link>
           );
         })}
       </div>
